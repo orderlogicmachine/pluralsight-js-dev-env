fc1590af5143d09332cb51f86075d652860fe238
JD D'Annunzio <vulcancannon@gmail.com>
2022-02-22 18:29:54 -0500

    Final Testing & Continuous Integration changes

--- src/index.test.js
+++ src/index.test.js
@@ -1,20 +1,21 @@
-import {expect} from 'chai';
+import { expect } from "chai";
 import jsdom from "jsdom";
-import fs from 'fs';
+import fs from "fs";
 
-describe('Our first test', () => {
-    it('should pass', () => {
-        expect(true).to.equal(true);
-    });
+describe("Our first test", () => {
+  it("should pass", () => {
+    expect(true).to.equal(true);
+  });
 });
 
-describe('index.html', () => {
-    it('should say hello', () => {
-        const index = fs.readFileSync("./src/index.html", "utf-8");
-        const {JSDOM} = jsdom;
-        const dom = new JSDOM(index);
-        const h1 = dom.window.document.getElementsByTagName("h1")[0];
-        expect(h1.innerHTML).to.equal("Hello World!");
-        dom.window.close();
+describe("index.html", () => {
+  it("should have h1 reading Users", () => {
+    const index = fs.readFileSync("./src/index.html", "utf-8");
+    jsdom.env(index, function (err, window) {
+      const h1 = window.document.getElementsByTagName("h1")[0];
+      expect(h1.innerHTML).to.equal("Users");
+      done();
+      window.close();
     });
-});
+  })
+})
